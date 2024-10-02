c0ecf75dae6523f0df4438140f5587fd667d1e60
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2024-10-01 21:33:46 -0500

    Update Dec_to_Bin.ino
    Added a function that instead of returning an array pointer returns the binary conversion as a string.

--- Avionics Code/Dec_to_Bin.ino
+++ Avionics Code/Dec_to_Bin.ino
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include <string>
 using namespace std;
 
 void setup() {
@@ -34,6 +35,24 @@ int* dec_to_binary(float my_dec, int my_bit){
   // }
   // my_arr[7] = 1;
   return my_arr;
+}
+
+// This returns the decimal as a string but in binary
+string decimal_to_binary(float my_dec, int my_bit){
+    int my_dec_int = static_cast<int>(round(my_dec));
+    int my_rem;
+    string my_bin_str;
+    for (int i = my_bit - 1; i >= 0; i--){
+        int my_exp = pow(2.0, i);
+        my_rem = my_dec_int % my_exp;
+        if (my_dec_int >= my_exp){
+            my_bin_str += "1";
+        } else if (my_dec_int < my_exp){
+            my_bin_str += "0";
+        }
+        my_dec_int = my_rem;
+    }
+    return my_bin_str;
 }
 
 

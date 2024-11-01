39c093fa55a57742adabca5bfb03dcf91824b596
TheFlyingPizzaHuose <85021898+TheFlyingPizzaHuose@users.noreply.github.com>
2024-10-31 21:15:55 -0500

    Add files via upload

--- Ground Station/Ground_Station.ino
+++ Ground Station/Ground_Station.ino
@@ -33,12 +33,42 @@ const int bitArrayLength = 136;
 static int bitArray[bitArrayLength] = {};
 const int charArrayLegnth = bitArrayLength/8;
 
+int errorCodes[32] = {}; //Check document for error code list
+//Error codes
+const int PRGM_ERR = 0,
+    ALT_OUT_RANGE = 1,
+    LAT_OUT_RANGE = 2,
+    LONG_OUT_RANGE = 3,
+    VEL_OUT_RANGE = 4,
+    ORIENT_X_OUT_RANGE = 5,
+    ORIENT_Y_OUT_RANGE = 6,
+    BMP280_ERR_DAT = 7,
+    SAMM8Q_ERR_DAT = 8,
+    MPU6050_ERR_DAT = 9,
+    BMP180_1_ERR_DAT = 10,
+    BMP180_2_ERR_DAT = 11,
+    AS5600_1_ERR_DAT = 12,
+    AS5600_2_ERR_DAT = 13,
+    TELEM_PWR_FAULT = 14,
+    VIDEO_PWR_FAULT = 15,
+    MAIN_PWR_FAULT = 16,
+    BMP280_FAIL = 17,
+    SAMM8Q_FAIL = 18,
+    MPU6050_FAIL = 19,
+    BMP180_1_FAIL = 20,
+    BMP180_2_FAIL = 21,
+    AS5600_1_FAIL = 22,
+    AS5600_2_FAIL = 23,
+    SD_FAIL = 24,
+    RFD900_FAIL = 25,
+    RFM9X_FAIL = 26,
+    ADXL345_FAIL = 27,
+    LSM9SD1_FAIL = 28;
+
 void setup() {
     Serial.begin(57600); // Start serial communication at 57600 baud rate
     rfSerial.begin(57600);
     Serial.println("Ground Station Ready!");
-    Serial.println("Below are the commands for the avionics system:");
-    Serial.println("1: Low Power Mode\n2: Normal Power Mode\n3: High Data Transfer\n4: Low Data Transfer\n5: Redo Startup Sequence\n6: Report battery voltage\n7: Ready for launch");
 }
 
 char lastData = 0;
@@ -48,26 +78,25 @@ int msg_recieved = false;
 void loop() {
   static char charArray[17] = {};
   if (rfSerial.available()) {
-      char data = rfSerial.read(); // Read from software serial
-      charArray[message_index] = data;
-      //Serial.print(data);
-      if(data == 'L'){//Checks if end characters are present
-        if(message_index == 16 && lastData == 'R'){
-          msg_recieved = true;
-          //Serial.println();
-          for(int i = 0; i<17; i++){
-            int* bins = uint_to_binary(charArray[i]);
-            for(int x = 0; x<8; x++){
-              bitArray[i*8 + x] = *(bins+x);
-              //Serial.print(bitArray[i*8 + x]);
-            }
+    char data = rfSerial.read(); // Read from software serial
+    charArray[message_index] = data;
+    if(data == 'L'){//Checks if end characters are present
+      if(message_index == 16 && lastData == 'R'){
+        msg_recieved = true;
+        //Serial.println();
+        for(int i = 0; i<17; i++){
+          int* bins = uint_to_binary(charArray[i]);
+          for(int x = 0; x<8; x++){
+            bitArray[i*8 + x] = *(bins+x);
+            //Serial.print(bitArray[i*8 + x]);
           }
         }
-        message_index = 0;
-      }else{
-        message_index++;
       }
-      lastData = data;
+      message_index = 0;
+    }else{
+      message_index++;
+    }
+    lastData = data;
   }
   if(msg_recieved){
     int bit_arr_read_pos = 0;
@@ -76,6 +105,9 @@ void loop() {
       float my_sum = 0;
       int my_index = 0;
       for(int x = bitLengthList[i]-1; x>=0; x--){//Converts bits back into valueEllie McGchee
+        if(i == 8){
+          errorCodes[31 - my_index] = bitArray[bit_arr_read_pos+x];
+        }
         my_sum = pow(2, my_index) * bitArray[bit_arr_read_pos+x]+ my_sum;
         my_index++;
       }
@@ -94,7 +126,8 @@ void loop() {
         case 6: Serial.print(" OREINT_X: "); Serial.print(telemetry[i]);break;
         case 7: Serial.print(" OREINT_Y: "); Serial.print(telemetry[i]);break;
         case 8://Deal with error Codes
-          Serial.print(" ERROR-DETECTED"); 
+          Serial.print("  ");
+          printErr(); 
           break;
         case 9: //Event
           Serial.print(" EVENT-PLACEHOLDER"); 
@@ -109,15 +142,27 @@ void loop() {
   
   last_time = millis();
   if (Serial.available()) {
-      char data = Serial.read() - '0';
-      if(data == 0xFF){
+      char data = Serial.read();
+      if(data == 'F'){
+          data = 0xff;
           rfSerial.print(data);
+      }else if(data == 'h'){
+        printCommands();
       }else{
         for(int i = 0; i < 10; i++){
+          data = data - '0';
           rfSerial.print(data);
         }
       }
   }
+}
+
+void printCommands(){
+  Serial.println();
+  Serial.println("Below are the commands for the avionics system:");
+  Serial.println("1: Low Power Mode\n2: Normal Power Mode\n3: High Data Transfer");
+  Serial.println("4: Low Data Transfer\n5: Redo Startup Sequence\n6: Report battery voltage");
+  Serial.println("7: Ready for launch\n8: Toggle Debug\nF: Shutdown");
 }
 
 void processData(String data) {
@@ -157,3 +202,40 @@ float binary_to_dec(int my_bit_size, int* my_arr){//Ellie McGchee
 }//end binary to decimal
 
 
+void printErr(){
+  for(int i = 0; i < 32; i++){
+    int check = i + errorCodes[i]*32;
+    switch(check){
+      case PRGM_ERR+32:Serial.print("PRGM_ERR  ");break;
+      case ALT_OUT_RANGE+32:Serial.print("ALT_OUT_RANGE  ");break;
+      case LAT_OUT_RANGE+32:Serial.print("LAT_OUT_RANGE  ");break;
+      case LONG_OUT_RANGE + 32: Serial.print("LONG_OUT_RANGE  "); break;
+      case VEL_OUT_RANGE + 32: Serial.print("VEL_OUT_RANGE  "); break;
+      case ORIENT_X_OUT_RANGE + 32: Serial.print("ORIENT_X_OUT_RANGE  "); break;
+      case ORIENT_Y_OUT_RANGE + 32: Serial.print("ORIENT_Y_OUT_RANGE  "); break;
+      case BMP280_ERR_DAT + 32: Serial.print("BMP280_ERR_DAT  "); break;
+      case SAMM8Q_ERR_DAT + 32: Serial.print("SAMM8Q_ERR_DAT  "); break;
+      case MPU6050_ERR_DAT + 32: Serial.print("MPU6050_ERR_DAT  "); break;
+      case BMP180_1_ERR_DAT + 32: Serial.print("BMP180_1_ERR_DAT  "); break;
+      case BMP180_2_ERR_DAT + 32: Serial.print("BMP180_2_ERR_DAT  "); break;
+      case AS5600_1_ERR_DAT + 32: Serial.print("AS5600_1_ERR_DAT  "); break;
+      case AS5600_2_ERR_DAT + 32: Serial.print("AS5600_2_ERR_DAT  "); break;
+      case TELEM_PWR_FAULT + 32: Serial.print("TELEM_PWR_FAULT  "); break;
+      case VIDEO_PWR_FAULT + 32: Serial.print("VIDEO_PWR_FAULT  "); break;
+      case MAIN_PWR_FAULT + 32: Serial.print("MAIN_PWR_FAULT  "); break;
+      case BMP280_FAIL + 32: Serial.print("BMP280_FAIL  "); break;
+      case SAMM8Q_FAIL + 32: Serial.print("SAMM8Q_FAIL  "); break;
+      case MPU6050_FAIL + 32: Serial.print("MPU6050_FAIL  "); break;
+      case BMP180_1_FAIL + 32: Serial.print("BMP180_1_FAIL  "); break;
+      case BMP180_2_FAIL + 32: Serial.print("BMP180_2_FAIL  "); break;
+      case AS5600_1_FAIL + 32: Serial.print("AS5600_1_FAIL  "); break;
+      case AS5600_2_FAIL + 32: Serial.print("AS5600_2_FAIL  "); break;
+      case SD_FAIL + 32: Serial.print("SD_FAIL  "); break;
+      case RFD900_FAIL + 32: Serial.print("RFD900_FAIL  "); break;
+      case RFM9X_FAIL + 32: Serial.print("RFM9X_FAIL  "); break;
+      case ADXL345_FAIL + 32: Serial.print("ADXL345_FAIL  "); break;
+      case LSM9SD1_FAIL + 32: Serial.print("LSM9SD1_FAIL  "); break;
+    }
+  }
+  Serial.print(" ");
+}

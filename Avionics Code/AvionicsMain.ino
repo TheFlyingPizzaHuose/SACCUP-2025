b027e36cfc15795b1665bf57776a545bbf5d8ecb
TheFlyingPizzaHuose <85021898+TheFlyingPizzaHuose@users.noreply.github.com>
2025-03-22 13:39:51 -0500

    Add files via upload

--- Avionics Code/AvionicsMain.ino
+++ Avionics Code/AvionicsMain.ino
@@ -865,21 +865,13 @@ void TCA9548A(uint8_t bus) {     // Select I2C Bus On I2C Multiplexer (if used)
 }  //end TCA9548A
 void setupLSM() {  //Ryan Santiago
   // 1.) Set the accelerometer range
-  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G, lsm.LSM9DS1_ACCELDATARATE_10HZ);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G, lsm.LSM9DS1_ACCELDATARATE_119HZ);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G, lsm.LSM9DS1_ACCELDATARATE_476HZ);
-  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G, lsm.LSM9DS1_ACCELDATARATE_952HZ);
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G, lsm.LSM9DS1_ACCELDATARATE_952HZ); //Options 2/4/8/16G 10/119/476/952Hz
 
   // 2.) Set the magnetometer sensitivity
-  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
-  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS); //Options 4/8/12/16
 
   // 3.) Setup the gyroscope
-  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
-  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
-  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS); //Options 245/500/2000
 }  //end setupLSM
 void setupMPU6050(){
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);//Options: 2/4/8/16

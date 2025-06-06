7d0d5368ccf8673bcd0849899bbe032344ac3d62
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2025-06-06 17:47:07 -0500

    Update AvionicsMain.ino

--- Avionics Code/AvionicsMain.ino
+++ Avionics Code/AvionicsMain.ino
@@ -347,9 +347,11 @@ void loop() {
     runKalmanFilter filter;
     float x_meters = lat_to_meters(GPS_LAT);
     float y_meters = lon_meters(GPS_LON)
-    filter.updateData(x_meters, y_meters, BMP280_ALT, BMP180_1_ALT, BMP180_2_ALT, MPU_AX, MPU_AY, MPU_AZ, 
-      LSM_AX, LSM_AY, LSM_AZ, ADXL345_AX, ADXL345_AY, ADXL_AZ, LSM_GX, LSM_GY, LSM_GZ, 
-      MPU_GX, MPU_GY, MPU_GZ);
+    
+  
+    filter.updateData(x_meters, y_meters, BMP280_ALT, BMP180_1_ALT, BMP180_1_PRESS, BMP180_2_PRESS, BMP280_PRESS,
+      MPU_AX, MPU_AY, MPU_AZ, LSM_AX, LSM_AY, LSM_AZ, ADXL345_AX, ADXL345_AY, ADXL_AZ, filter.state[12], filter.state[13],
+      filter.state[14], filter.state[15], LSM_GX, LSM_GY, LSM_GZ, MPU_GX, MPU_GY, MPU_GZ);
     filter.updateStateTransition();
     filter.updateMeasurement();
     filter.updateState_and_Covar();

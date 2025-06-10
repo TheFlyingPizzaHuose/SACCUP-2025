e2a382fdbca904ccb3659d4716d9242bb67323ca
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2025-06-10 17:42:16 -0500

    Update kalman_filter.h

--- Avionics Code/kalman_filter.h
+++ Avionics Code/kalman_filter.h
@@ -6,8 +6,8 @@
 using namespace std;
 
 // This is good ! ----------------------------------------------------------
-vector<vector<double>> swap_rows(vector<vector<double>> myvec, int n, int m){
-  vector<double> temp = myvec[n];
+vector<vector<float>> swap_rows(vector<vector<float>> myvec, int n, int m){
+  vector<float> temp = myvec[n];
   myvec[n] = myvec[m];
   myvec[m] = temp;
 
@@ -15,8 +15,8 @@ vector<vector<double>> swap_rows(vector<vector<double>> myvec, int n, int m){
 }
 
 // This is good ! ----------------------------------------------------------
-vector<vector<double>> inverse(int n, vector<vector<double>> mat){
-  vector<vector<double>> I(n, vector<double>(n,0.0));
+vector<vector<float>> inverse(int n, vector<vector<float>> mat){
+  vector<vector<float>> I(n, vector<float>(n,0.0));
 
   // We have the indentity matrix
   for (int i = 0; i < n; i++){
@@ -43,7 +43,7 @@ vector<vector<double>> inverse(int n, vector<vector<double>> mat){
   }
   
 
-  double ratio;
+  float ratio;
   
   // Now we row reduce that shit !! Make sure it can handle edge cases
   for (int i = 0; i < n; i++){
@@ -85,10 +85,10 @@ vector<vector<double>> inverse(int n, vector<vector<double>> mat){
 
 
 // This is good! --------------------------------------------------
-vector<vector<double>> transposeMat(vector<vector<double>> mat) {
+vector<vector<float>> transposeMat(vector<vector<float>> mat) {
   int n = mat.size();
   int m = mat[0].size();
-  vector<vector<double>> my_result(m, vector<double>(n, 0));
+  vector<vector<float>> my_result(m, vector<float>(n, 0));
   for (int i = 0; i < my_result.size(); i++) {
     for (int j = 0; j < my_result[i].size(); j++) {
       my_result[i][j] = mat[j][i];
@@ -98,10 +98,10 @@ vector<vector<double>> transposeMat(vector<vector<double>> mat) {
 }
 
 // This is good ! -----------------------------------------------
-vector<vector<double>> matrix_addition(vector<vector<double>> mat1, vector<vector<double>> mat2){
+vector<vector<float>> matrix_addition(vector<vector<float>> mat1, vector<vector<float>> mat2){
     int m = mat1.size();
     int n = mat1[0].size();
-    vector<vector<double>> my_result(m, vector<double>(n, 0));
+    vector<vector<float>> my_result(m, vector<float>(n, 0));
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         my_result[i][j] = mat1[i][j] + mat2[i][j];
@@ -111,7 +111,7 @@ vector<vector<double>> matrix_addition(vector<vector<double>> mat1, vector<vecto
   }
 
 // This is good ! -----------------------------------------------------------------------
-vector<vector<double>> matrix_multiplication(vector<vector<double>> mat1, vector<vector<double>> mat2){
+vector<vector<float>> matrix_multiplication(vector<vector<float>> mat1, vector<vector<float>> mat2){
     int r1 = mat1.size();
     int c1 = mat1[0].size();
     int r2 = mat2.size();
@@ -120,7 +120,7 @@ vector<vector<double>> matrix_multiplication(vector<vector<double>> mat1, vector
       cout << "Aborting Program" << endl;
       abort();
     }
-    vector<vector<double>> my_result(r1, vector<double>(c2, 0));
+    vector<vector<float>> my_result(r1, vector<float>(c2, 0));
     for (int i = 0; i < r1; i++) {
       for (int j = 0; j < c2; j++) {
         my_result[i][j] = 0;
@@ -133,10 +133,10 @@ vector<vector<double>> matrix_multiplication(vector<vector<double>> mat1, vector
 }
 
 // This is good ! -----------------------------------------------------------------------------
-vector<vector<double>> subtractMat(vector<vector<double>> mat1, vector<vector<double>> mat2) {
+vector<vector<float>> subtractMat(vector<vector<float>> mat1, vector<vector<float>> mat2) {
   int r1 = mat1.size();
   int c1 = mat1[0].size();
-  vector<vector<double>> my_result(r1, vector<double>(c1, 0));
+  vector<vector<float>> my_result(r1, vector<float>(c1, 0));
   for (int i = 0; i < my_result.size(); i++) {
       for (int j = 0; j < my_result[i].size(); j++) {
         my_result[i][j] = mat1[i][j] - mat2[i][j];
@@ -148,14 +148,14 @@ vector<vector<double>> subtractMat(vector<vector<double>> mat1, vector<vector<do
 // This class is good ! ---------------------------------------------------------------------------------------------
 class KalmanFilter {
   public:
-      vector<vector<double>> A_matrix;
-      vector<vector<double>> H_matrix;
-      vector<vector<double>> Q_matrix;
-      vector<vector<double>> R_matrix;
+      vector<vector<float>> A_matrix;
+      vector<vector<float>> H_matrix;
+      vector<vector<float>> Q_matrix;
+      vector<vector<float>> R_matrix;
       
 
-      KalmanFilter(vector<vector<double>> A, vector<vector<double>> H, 
-          vector<vector<double>> Q, vector<vector<double>> R){
+      KalmanFilter(vector<vector<float>> A, vector<vector<float>> H, 
+          vector<vector<float>> Q, vector<vector<float>> R){
           A_matrix = A;
           H_matrix = H;
           Q_matrix = Q;
@@ -163,53 +163,53 @@ class KalmanFilter {
       }
       ~KalmanFilter(){}
       
-      vector<vector<double>> predict_state(vector<vector<double>> state_0){
+      vector<vector<float>> predict_state(vector<vector<float>> state_0){
         return matrix_multiplication(A_matrix, state_0);
         }
         
-      vector<vector<double>> predict_error_covariance(vector<vector<double>> err_co_0){
-          vector<vector<double>> result = matrix_multiplication(A_matrix, err_co_0);
+      vector<vector<float>> predict_error_covariance(vector<vector<float>> err_co_0){
+          vector<vector<float>> result = matrix_multiplication(A_matrix, err_co_0);
           return matrix_addition(matrix_multiplication(result, transposeMat(A_matrix)), Q_matrix);
         }
 
       
-      vector<vector<double>> kalman_gain(vector<vector<double>> pred_co){
-          vector<vector<double>> result = matrix_multiplication(H_matrix, pred_co);
-          vector<vector<double>> result_two = transposeMat(H_matrix); 
-          vector<vector<double>> result_three = matrix_multiplication(result, result_two);
-          vector<vector<double>> result_four = matrix_addition(result_three, R_matrix);
-          vector<vector<double>> result_five = matrix_multiplication(pred_co, result_two);
-          vector<vector<double>> result_six = inverse(result_four.size(), result_four); //this is the problem function :(
+      vector<vector<float>> kalman_gain(vector<vector<float>> pred_co){
+          vector<vector<float>> result = matrix_multiplication(H_matrix, pred_co);
+          vector<vector<float>> result_two = transposeMat(H_matrix); 
+          vector<vector<float>> result_three = matrix_multiplication(result, result_two);
+          vector<vector<float>> result_four = matrix_addition(result_three, R_matrix);
+          vector<vector<float>> result_five = matrix_multiplication(pred_co, result_two);
+          vector<vector<float>> result_six = inverse(result_four.size(), result_four); //this is the problem function :(
 
           return matrix_multiplication(result_five, result_six);
 
         }
-      vector<vector<double>> estimate(vector<vector<double>> kal_gain, vector<vector<double>> pred_state, vector<vector<double>> z_0){
-          vector<vector<double>> result = subtractMat(z_0, matrix_multiplication(H_matrix, pred_state));
+      vector<vector<float>> estimate(vector<vector<float>> kal_gain, vector<vector<float>> pred_state, vector<vector<float>> z_0){
+          vector<vector<float>> result = subtractMat(z_0, matrix_multiplication(H_matrix, pred_state));
           return matrix_addition(pred_state, matrix_multiplication(kal_gain, result));
         }
         
-      vector<vector<double>> compute_error_covariance(vector<vector<double>> pred_co, vector<vector<double>> kal_gain){
-          vector<vector<double>> result = matrix_multiplication(kal_gain, H_matrix);
+      vector<vector<float>> compute_error_covariance(vector<vector<float>> pred_co, vector<vector<float>> kal_gain){
+          vector<vector<float>> result = matrix_multiplication(kal_gain, H_matrix);
           return subtractMat(pred_co, matrix_multiplication(result, pred_co));
       }
 
-      vector<vector<double>> run_kalman_filter_estimate(vector<vector<double>> err_co_estimate, 
-          vector<vector<double>> state_0, vector<vector<double>> measurement){
-          vector<vector<double>> predicted_state = predict_state(state_0);
-          vector<vector<double>> predicted_err_co = predict_error_covariance(err_co_estimate);
-          vector<vector<double>> kal_gain = kalman_gain(predicted_err_co);
-          vector<vector<double>> est_state = estimate(kal_gain, predicted_state, measurement);
-          vector<vector<double>> comp_err_co = compute_error_covariance(predicted_err_co, kal_gain);
+      vector<vector<float>> run_kalman_filter_estimate(vector<vector<float>> err_co_estimate, 
+          vector<vector<float>> state_0, vector<vector<float>> measurement){
+          vector<vector<float>> predicted_state = predict_state(state_0);
+          vector<vector<float>> predicted_err_co = predict_error_covariance(err_co_estimate);
+          vector<vector<float>> kal_gain = kalman_gain(predicted_err_co);
+          vector<vector<float>> est_state = estimate(kal_gain, predicted_state, measurement);
+          vector<vector<float>> comp_err_co = compute_error_covariance(predicted_err_co, kal_gain);
           return est_state;
       }
 
-      vector<vector<double>> run_kalman_filter_covar(vector<vector<double>> err_co_estimate, 
-        vector<vector<double>> state_0, vector<vector<double>> measurement){
-        vector<vector<double>> predicted_state = predict_state(state_0);
-        vector<vector<double>> predicted_err_co = predict_error_covariance(err_co_estimate);
-        vector<vector<double>> kal_gain = kalman_gain(predicted_err_co);
-        vector<vector<double>> comp_err_co = compute_error_covariance(predicted_err_co, kal_gain);
+      vector<vector<float>> run_kalman_filter_covar(vector<vector<float>> err_co_estimate, 
+        vector<vector<float>> state_0, vector<vector<float>> measurement){
+        vector<vector<float>> predicted_state = predict_state(state_0);
+        vector<vector<float>> predicted_err_co = predict_error_covariance(err_co_estimate);
+        vector<vector<float>> kal_gain = kalman_gain(predicted_err_co);
+        vector<vector<float>> comp_err_co = compute_error_covariance(predicted_err_co, kal_gain);
         return comp_err_co;
     }
 };
@@ -220,43 +220,43 @@ class runKFIMU{
 
   public:
 
-    vector<vector<double>> A = {{1,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0},
+    vector<vector<float>> A = {{1,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0},
                                 {0,0,0,1,0,0,0,0,0},{0,0,0,0,1,0,0,0,0},{0,0,0,0,0,1,0,0,0},
                                 {0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,1}};
 
-    vector<vector<double>> H = {{1,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0},
+    vector<vector<float>> H = {{1,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0},
                                 {0,0,0,1,0,0,0,0,0},{0,0,0,0,1,0,0,0,0},{0,0,0,0,0,1,0,0,0},
                                 {0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,1}};
 
-    vector<vector<double>> Q = {{0.001,0,0,0,0,0,0,0,0},{0,0.001,0,0,0,0,0,0,0},{0,0,0.001,0,0,0,0,0,0},
+    vector<vector<float>> Q = {{0.001,0,0,0,0,0,0,0,0},{0,0.001,0,0,0,0,0,0,0},{0,0,0.001,0,0,0,0,0,0},
                                 {0,0,0,0.001,0,0,0,0,0},{0,0,0,0,0.001,0,0,0,0},{0,0,0,0,0,0.001,0,0,0},
                                 {0,0,0,0,0,0,0.001,0,0},{0,0,0,0,0,0,0,0.001,0},{0,0,0,0,0,0,0,0,0.001}};
 
-    vector<vector<double>> P = {{0.3,0,0,0,0,0,0,0,0},{0,0.3,0,0,0,0,0,0,0},{0,0,0.3,0,0,0,0,0,0},
+    vector<vector<float>> P = {{0.3,0,0,0,0,0,0,0,0},{0,0.3,0,0,0,0,0,0,0},{0,0,0.3,0,0,0,0,0,0},
                                 {0,0,0,0.3,0,0,0,0,0},{0,0,0,0,0.3,0,0,0,0},{0,0,0,0,0,0.3,0,0,0},
                                 {0,0,0,0,0,0,0.3,0,0},{0,0,0,0,0,0,0,0.3,0},{0,0,0,0,0,0,0,0,0.3}};
     
 
-    vector<vector<double>> R = {{5,0,0,0,0,0,0,0,0},{0,5,0,0,0,0,0,0,0},{0,0,5,0,0,0,0,0,0},
+    vector<vector<float>> R = {{5,0,0,0,0,0,0,0,0},{0,5,0,0,0,0,0,0,0},{0,0,5,0,0,0,0,0,0},
                                 {0,0,0,5,0,0,0,0,0},{0,0,0,0,5,0,0,0,0},{0,0,0,0,0,5,0,0,0},
                                 {0,0,0,0,0,0,5,0,0},{0,0,0,0,0,0,5,0,0},{0,0,0,0,0,0,0,0,5}};
                                                                
-    vector<vector<double>> R_fuse = {{5,0,0},{0,5,0},{0,0,5}};
-    vector<vector<double>> R_end = {{0.3,0,0},{0,0.3,0},{0,0,0.3}};
+    vector<vector<float>> R_fuse = {{5,0,0},{0,5,0},{0,0,5}};
+    vector<vector<float>> R_end = {{0.3,0,0},{0,0.3,0},{0,0,0.3}};
 
-    vector<vector<double>> z = {{0},{0},{0},{0},{0},{0},{0},{0},{0}};
-    vector<vector<double>> a1 = {{0},{0},{0}};
-    vector<vector<double>> a2 = {{0},{0},{0}};
-    vector<vector<double>> a3 = {{0},{0},{0}};
-    vector<vector<double>> g1 = {{0},{0},{0}};
-    vector<vector<double>> g2 = {{0},{0},{0}};
-    vector<vector<double>> M = {{0},{0},{0}};
-    vector<vector<double>> accel = {{0},{0},{0}};
-    vector<vector<double>> gyro = {{0},{0},{0}};
-    vector<vector<double>> x = {{0},{0},{0},{0},{0},{0},{0},{0},{0}};
+    vector<vector<float>> z = {{0},{0},{0},{0},{0},{0},{0},{0},{0}};
+    vector<vector<float>> a1 = {{0},{0},{0}};
+    vector<vector<float>> a2 = {{0},{0},{0}};
+    vector<vector<float>> a3 = {{0},{0},{0}};
+    vector<vector<float>> g1 = {{0},{0},{0}};
+    vector<vector<float>> g2 = {{0},{0},{0}};
+    vector<vector<float>> M = {{0},{0},{0}};
+    vector<vector<float>> accel = {{0},{0},{0}};
+    vector<vector<float>> gyro = {{0},{0},{0}};
+    vector<vector<float>> x = {{0},{0},{0},{0},{0},{0},{0},{0},{0}};
 
-    void updateData(double ax1, double ax2, double ax3, double ay1, double ay2, double ay3, double az1, double az2, double az3,
-                    double gx1, double gx2, double gy1, double gy2, double gz1, double gz2, double mx, double my, double mz){
+    void updateData(float ax1, float ax2, float ax3, float ay1, float ay2, float ay3, float az1, float az2, float az3,
+                    float gx1, float gx2, float gy1, float gy2, float gz1, float gz2, float mx, float my, float mz){
       a1[0][0] = ax1;
       a1[1][0] = ay1;
       a1[2][0] = az1;
@@ -315,36 +315,36 @@ class runKFOrientation {
   private:
 
   public:
-    vector<vector<double>> A = {{1,0,0,0},
+    vector<vector<float>> A = {{1,0,0,0},
                                 {0,1,0,0},
                                 {0,0,1,0},
                                 {0,0,0,1}};
-    vector<vector<double>> Q = {{0.001,0,0,0},
+    vector<vector<float>> Q = {{0.001,0,0,0},
                                 {0,0.001,0,0},
                                 {0,0,0.001,0},
                                 {0,0,0,0.001}};
-    vector<vector<double>> H = {{0,0,0,0}, 
+    vector<vector<float>> H = {{0,0,0,0}, 
                                 {0,0,0,0},
                                 {0,0,0,0}};
-    vector<vector<double>> R = {{1.0,0,0},
+    vector<vector<float>> R = {{1.0,0,0},
                                 {0,1.0,0},
                                 {0,0,1.0}};
-    vector<vector<double>> z = {{0},{0},{0}};
-    vector<vector<double>> x = {{0},{0},{0},{0}};
-    vector<vector<double>> P = {{0.001,0,0,0},
+    vector<vector<float>> z = {{0},{0},{0}};
+    vector<vector<float>> x = {{0},{0},{0},{0}};
+    vector<vector<float>> P = {{0.001,0,0,0},
                                 {0,0.001,0,0},
                                 {0,0,0.001,0},
                                 {0,0,0,0.001}};
-    vector<vector<double>> x_loc = {{0},{0},{0},{0}};
-    vector<vector<double>> P_loc = {{0.001,0,0,0},
+    vector<vector<float>> x_loc = {{0},{0},{0},{0}};
+    vector<vector<float>> P_loc = {{0.001,0,0,0},
                                       {0,0.001,0,0},
                                       {0,0,0.001,0},
                                       {0,0,0,0.001}};
 
-    double gx, gy, gz;
+    float gx, gy, gz;
 
-    void updateData(double ax1, double ax2, double ax3, double ay1, double ay2, double ay3, double az1, double az2, double az3,
-                    double gx1, double gx2, double gy1, double gy2, double gz1, double gz2, double mx, double my, double mz){
+    void updateData(float ax1, float ax2, float ax3, float ay1, float ay2, float ay3, float az1, float az2, float az3,
+                    float gx1, float gx2, float gy1, float gy2, float gz1, float gz2, float mx, float my, float mz){
       runKFIMU obj;
       obj.updateData(ax1, ax2, ax3, ay1, ay2, ay3, az1, az2, az3, gx1, gx2, gy1, gy2, gz1, gz2, mx, my, mz);
       obj.fuseDataAcceleration_and_Gyro();
@@ -358,7 +358,7 @@ class runKFOrientation {
       z[2][0] = gz;
     }
     void update_state_transition(){
-      double t = 0.8;
+      float t = 0.8;
       A = {{t/2,  -t/2 * gx, -t/2 * gy, -t/2 * gz},         
           {t/2 * gx, t/2, t/2 * gz,  t/2 * gy},
           {t/2 * gy, -t/2 * gz, t/2, t/2 * gx},
@@ -371,24 +371,24 @@ class runKFOrientation {
       x = myObj.run_kalman_filter_covar(P, x, z);
       P = myObj.run_kalman_filter_covar(P, x, z);
 
-      vector<vector<double>> A_loc = {{1,0,0,0},
+      vector<vector<float>> A_loc = {{1,0,0,0},
                                       {0,1,0,0},
                                       {0,0,1,0},
                                       {0,0,0,1}};
-      vector<vector<double>> Q_loc = {{0.001,0,0,0},
+      vector<vector<float>> Q_loc = {{0.001,0,0,0},
                                     {0,0.001,0,0},
                                     {0,0,0.001,0},
                                     {0,0,0,0.001}};
-      vector<vector<double>> H_loc = {{1,0,0,0}, 
+      vector<vector<float>> H_loc = {{1,0,0,0}, 
                                     {0,1,0,0},
                                     {0,0,1,0},
                                     {0,0,0,1}};
-      vector<vector<double>> R_loc = {{3,0,0,0},
+      vector<vector<float>> R_loc = {{3,0,0,0},
                                     {0,3,0,0},
                                     {0,0,3,0},
                                     {0,0,0,3}};
 
-      vector<vector<double>> z_loc = {{0},{0},{0},{0}};
+      vector<vector<float>> z_loc = {{0},{0},{0},{0}};
 
       KalmanFilter quat(A_loc, Q_loc, H_loc, R_loc);
       x_loc = quat.run_kalman_filter_estimate(P_loc, x_loc, x);
